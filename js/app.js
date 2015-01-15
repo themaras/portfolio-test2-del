f17d9c4d9003f764f115d3f62427cf3e033e830d
Themistoklis Papaioannou <themaras3@gmail.com>
2015-01-15 21:10:05 +0200

    initial commit

---
+++ js/app.js
@@ -0,0 +1,53 @@
+$(document).ready(function() {
+  $('.ryu').mouseenter(function() {
+    $('.ryu-still').hide();
+    $('.ryu-ready').show();
+  })
+  .mouseleave(function() {
+    $('.ryu-ready').hide();
+    $('.ryu-still').show();
+  })
+  .mousedown(function() {
+    playHadouken();    
+    $('.ryu-ready').hide();
+    $('.ryu-throwing').show();
+    $('.hadouken').finish().show()
+    .animate(
+      {'left': '300px'},
+      500,
+      function() {
+        $(this).hide();
+        $(this).css('left', '-212px');
+      }
+    );
+  })
+  .mouseup(function() {
+    $('.ryu-throwing').hide();
+    $('.ryu-ready').show();
+  });
+  $(document).keydown(function(e) {
+  if (e.keyCode == 88){
+    playJammin();
+    $('.ryu-ready, .ryu-still, .ryu-throwing').hide();
+    $('.ryu-cool').show();
+    }
+  })
+  .keyup(function(e) {
+  if (e.keyCode == 88){
+    $('.ryu-cool, .ryu-ready, .ryu-throwing').hide();
+    $('.ryu-still').show();
+    }
+});
+});
+
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
+
+function playJammin () {
+  $('#cool')[0].volume = 0.5;
+  $('#cool')[0].load();
+  $('#cool')[0].play();
+ }
